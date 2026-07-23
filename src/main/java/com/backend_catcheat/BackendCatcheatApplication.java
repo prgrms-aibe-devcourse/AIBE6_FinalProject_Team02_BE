533ab365e642297cb2cc281dab297cb18253d628
Rakhyunn <krh0323@pukyong.ac.kr>
2026-07-23 16:47:52 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/backend_catcheat/BackendCatcheatApplication.java
@@ -0,0 +1,13 @@
+package com.backend_catcheat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCatcheatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCatcheatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend_catcheat/BackendCatcheatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_catcheat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCatcheatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
