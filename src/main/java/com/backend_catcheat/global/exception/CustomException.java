8d3ab63e1de8df530a584bf3c4eb02cf762bd832
Rakhyunn <krh0323@pukyong.ac.kr>
2026-07-26 12:16:08 +0900

    CATCHEAT-8 feat: 전역 예외처리 구현

---
+++ src/main/java/com/backend_catcheat/global/exception/CustomException.java
@@ -0,0 +1,17 @@
+package com.backend_catcheat.global.exception;
+
+import lombok.Getter;
+
+/**
+ * 도메인 로직에서 ErrorCode를 담아 던진다.
+ */
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
