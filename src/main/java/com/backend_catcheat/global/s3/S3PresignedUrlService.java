9d63c19be3444616ad005d864e3d456e422e4481
yunabin <yhb1020525@gmail.com>
2026-07-28 14:34:34 +0900

    CATCHEAT-6 refactor: 업로드 응답에서 publicUrl 제거

--- src/main/java/com/backend_catcheat/global/s3/S3PresignedUrlService.java
+++ src/main/java/com/backend_catcheat/global/s3/S3PresignedUrlService.java
@@ -105,8 +105,7 @@ public class S3PresignedUrlService {
 
         return new PresignedUploadResponseDTO.UploadTarget(
                 key,
-                presignedRequest.url().toString(),
-                buildPublicUrl(key)
+                presignedRequest.url().toString()
         );
     }
 
@@ -139,10 +138,6 @@ public class S3PresignedUrlService {
             case "image/heif" -> ".heif";
             default -> throw new CustomException(ErrorCode.INVALID_UPLOAD_FILE);
         };
-    }
-
-    private String buildPublicUrl(String key) {
-        return s3Properties.publicBaseUrl().replaceAll("/+$", "") + "/" + key;
     }
 
     private String extractKey(String objectLocation) {
--- src/main/java/com/backend_catcheat/global/s3/S3Properties.java
+++ src/main/java/com/backend_catcheat/global/s3/S3Properties.java
@@ -6,7 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public record S3Properties (
         String bucket,
         String region,
-        String publicBaseUrl,
         String accessKeyId,
         String secretAccessKey,
         String sessionToken
