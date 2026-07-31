b64f5aed5b342a108dd997a9216ad7ba30da3288
SHINJAEHEE-DEV <asjh011969@gmail.com>
2026-07-31 10:27:40 +0900

    CATCHEAT-20 feat:entity 작성중

---
+++ src/main/java/com/backend_catcheat/domain/challenge/entity/ChallengeParticipant.java
@@ -0,0 +1,4 @@
+package com.backend_catcheat.domain.challenge.entity;
+
+public class ChallengeParticipant {
+}
---
+++ src/main/java/com/backend_catcheat/domain/challenge/entity/ChallengeType.java
@@ -0,0 +1,6 @@
+package com.backend_catcheat.domain.challenge.entity;
+
+public enum ChallengeType {
+    FIRST_COME,
+    COLLECTION
+}
