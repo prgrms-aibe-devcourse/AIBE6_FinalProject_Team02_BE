e00af512325d2fd6a9b179b1b6d2d8afb91ecb8d
tke0329 <tke0329@gmail.com>
2026-08-08 22:59:18 +0900

    [CATCHEAT-45][fix] Security CORS 정책 수정
    * CATCHEAT-45/chore: CI 구축을 위한 terraform 및 .github 설정 파일 추가
    
    * CATCHEAT-45/fix: 코드래빗 피드백 반영
    
    * CATCHEAT-45/chore: CI 방식 수정 ODCI -> access key 재사용
    
    * CATCHEAT-45/fix: 주석 수정
    
    * CATCHEAT-45/chore: CD 구축 docker yml 설정
    
    * CATCHEAT-45/fix: Security CORS 수정
    
    * CATCHEAT-45/fix: localhost 제거

--- src/main/java/com/backend_catcheat/global/security/SecurityConfig.java
+++ src/main/java/com/backend_catcheat/global/security/SecurityConfig.java
@@ -82,7 +82,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000")); // 프론트 주소(운영 시 실제 도메인 추가)
+        config.setAllowedOrigins(List.of("https://projectjm.co.kr")); // 프론트 주소(운영 시 실제 도메인 추가)
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true); // 쿠키 전송 허용 (httpOnly 쿠키 인증에 필수)
